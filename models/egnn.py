89a8c41646013c9b001cecb2b557d5ca56cb7a30
zuoan77 <774548845@qq.com>
2025-12-16 10:47:20 +0800

    update

--- models/egnn.py
+++ models/egnn.py
@@ -129,7 +129,8 @@ class CrossAttentionBlock(Module):
         
         # FFN with residual
         ligand_h = ligand_h + self.ffn(self.layer_norm_ffn(ligand_h))
-        
+        # add debug print
+        print(f"Ligand: {ligand_h.shape}, Protein: {protein_h.shape}, Batch Mask: {batch_mask.shape}")
         return ligand_h
 
 
@@ -230,8 +231,6 @@ class EfficientCrossAttentionBlock(Module):
         # FFN with residual
         ligand_h = ligand_h + self.ffn(self.layer_norm_ffn(ligand_h))
         
-        # 临时调试用
-        print(f"Ligand: {ligand_h.shape}, Protein: {protein_h.shape}, Batch Mask: {batch_mask.shape}")
         return ligand_h
 
 
